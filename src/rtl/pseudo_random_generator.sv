90b9cef4e454d984de55173bb58925304fc43812
ralex2304 <ralex2304@gmail.com>
2024-11-08 15:33:06 +0300

    Fix reset bug in prng

--- src/rtl/pseudo_random_generator.sv
+++ src/rtl/pseudo_random_generator.sv
@@ -1,12 +1,12 @@
 module pseudo_random_generator (
     input logic        clk,
-    input logic        reset,
+    input logic        arst_n,
     output logic [7:0] o_prng
 );
     logic [7:0] prng_buffer;
 
-    always_ff @(posedge clk or posedge reset) begin
-        if (reset) begin
+    always_ff @(posedge clk or negedge arst_n) begin
+        if (!arst_n) begin
             prng_buffer <= 8'hDA; // Must not be 0 i thing probably idk
         end else begin
             prng_buffer <= {prng_buffer[6:0], prng_buffer[7] ^
