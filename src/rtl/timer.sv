2004c6204a7a0c094f0fcac87241699d36adc93c
fogInSingularity <vladimirnaumov979@gmail.com>
2024-11-08 16:02:28 +0300

    add timer submodule

---
+++ src/rtl/timer.sv
@@ -0,0 +1,34 @@
+module timer # (
+    parameter TIMER_WIDTH = 16,
+    parameter CLK_FREQ    = 100_000_000
+)(
+    input logic                         clk             ,
+    input logic                         rst_n           ,
+    input logic                         i_reset_timer   ,
+
+    output logic [TIMER_WIDTH - 1 : 0]  o_current_time  
+);
+
+logic [TIMER_WIDTH - 1 : 0] timer;
+
+localparam CLK_TIMER_WIDTH = 32;
+logic [CLK_TIMER_WIDTH - 1 : 0] clk_timer;
+
+always @ (posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        timer <= 0;
+        clk_timer <= 0;
+    end else begin
+        if (i_reset_timer) begin
+            timer <= 0;
+        end
+        
+        clk_timer <= clk_timer + 1;
+
+        if (clk_timer == CLK_FREQ) begin
+            timer <= timer + 1;
+        end
+    end
+end
+
+endmodule
