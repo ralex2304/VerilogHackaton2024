f022081f2265c9f1bcad640c6e7a89a7b338df07
nniikon <kolya.kasparov@gmail.com>
2024-11-08 14:57:12 +0300

    fix signed bug

--- src/rtl/graphic.sv
+++ src/rtl/graphic.sv
@@ -31,8 +31,8 @@ module graphic # (
     logic signed [ 9:0] v_ball_pos_diff;
     logic        [21:0] ball_dist_squared;
 
-    assign h_ball_pos_diff = $signed(i_h_coord) - $signed(i_screen_ball_x);
-    assign v_ball_pos_diff = $signed(i_v_coord) - $signed(i_screen_ball_y);
+    assign h_ball_pos_diff = signed'(i_h_coord) - signed'(i_screen_ball_x);
+    assign v_ball_pos_diff = signed'(i_v_coord) - signed'(i_screen_ball_y);
 
     assign ball_dist_squared = h_ball_pos_diff * h_ball_pos_diff + 
                                v_ball_pos_diff * v_ball_pos_diff;
